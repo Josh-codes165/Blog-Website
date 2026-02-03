dc95a22af2db872b3f07d801b161e538d5b3cae9
Joshua Okoronkwo <joshuaokoronkwo54@gmail.com>
2026-02-03 01:56:47 +0100

    Joshua Commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import Errorboundary from "./Components/Errorboundary.Jsx";
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <Errorboundary fallback = "Sonething went wrong">
-        <App />
-      </Errorboundary>
+      <App />
     </BrowserRouter>
   </StrictMode>,
 );
