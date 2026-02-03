a18d5b491c0277ad90700bfe7f92c7736c2ab12e
Joshua Okoronkwo <joshuaokoronkwo54@gmail.com>
2026-02-03 01:41:02 +0100

    Joshua Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,30 @@
 import React from "react";
 import { Suspense } from "react";
 import Suspenses from "./Components/Suspenses";
+import Errorboundary from "./Components/Errorboundary.Jsx";
 import { Routes, Route } from "react-router-dom";
-const About = React.lazy(() => import ("./pages/About"))
-const Blog = React.lazy(() => import ("./pages/Blog"))
-const BlogDetails = React.lazy(() => import ("./pages/BlogDetails"))
-const NavBar = React.lazy(() => import ("./Components/NavBar"))
-const Home = React.lazy(() => import ("./pages/Home"))
-
+const About = React.lazy(() => import("./pages/About"));
+const Blog = React.lazy(() => import("./pages/Blog"));
+const BlogDetails = React.lazy(() => import("./pages/BlogDetails"));
+const NavBar = React.lazy(() => import("./Components/NavBar"));
+const Home = React.lazy(() => import("./pages/Home"));
 
 function App() {
   return (
     <>
-    <NavBar />
-      <Suspense fallback={<Suspenses/>}>
-        <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/blogs" element={<Blog />} />
-        <Route path="/blogs/:id" element={<BlogDetails />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-      </Suspense>
+      <NavBar />
+      <Errorboundary fallback="Something went wrong">
+        <Suspense fallback={<Suspenses />}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/blogs" element={<Blog />} />
+            <Route path="/blogs/:id" element={<BlogDetails />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </Suspense>
+      </Errorboundary>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-
+import Errorboundary from "./Components/Errorboundary.Jsx";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <Errorboundary fallback = "Sonething went wrong">
+        <App />
+      </Errorboundary>
     </BrowserRouter>
   </StrictMode>,
 );
