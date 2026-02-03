8e741bb3eef8a4c49cf1fc1ab5fb5634d33f768d
Joshua Okoronkwo <joshuaokoronkwo54@gmail.com>
2026-02-03 01:53:18 +0100

    Joshua Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,28 @@
 import React from "react";
 import { Suspense } from "react";
 import Suspenses from "./Components/Suspenses";
-import Errorboundary from "./Components/Errorboundary.Jsx";
 import { Routes, Route } from "react-router-dom";
-const About = React.lazy(() => import("./pages/About"));
-const Blog = React.lazy(() => import("./pages/Blog"));
-const BlogDetails = React.lazy(() => import("./pages/BlogDetails"));
-const NavBar = React.lazy(() => import("./Components/NavBar"));
-const Home = React.lazy(() => import("./pages/Home"));
+const About = React.lazy(() => import ("./pages/About"))
+const Blog = React.lazy(() => import ("./pages/Blog"))
+const BlogDetails = React.lazy(() => import ("./pages/BlogDetails"))
+const NavBar = React.lazy(() => import ("./Components/NavBar"))
+const Home = React.lazy(() => import ("./pages/Home"))
+
 
 function App() {
   return (
     <>
-      <NavBar />
-      <Errorboundary fallback="Something went wrong">
-        <Suspense fallback={<Suspenses />}>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/blogs" element={<Blog />} />
-            <Route path="/blogs/:id" element={<BlogDetails />} />
-            <Route path="/about" element={<About />} />
-          </Routes>
-        </Suspense>
-      </Errorboundary>
+    <NavBar />
+      <Suspense fallback={<Suspenses/>}>
+        <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/blogs" element={<Blog />} />
+        <Route path="/blogs/:id" element={<BlogDetails />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+      </Suspense>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
